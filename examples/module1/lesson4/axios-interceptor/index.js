195d71a632dcb0d324821cd3fe3d1f981006a20a
grzegorz <grzegorz.szczesniak@firma.interia.pl>
2024-12-19 21:48:51 +0100

    lesson-4 axios interceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
 
-// Add a request interceptor
-axios.interceptors.request.use(function (config) {
+axios.interceptors.request.use((config) => {
+  config.metadata = { startTime: new Date() };
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
-// Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  // Do something with response data
+axios.interceptors.response.use((response) => {
+  const endTime = new Date();
+  const startTime = response?.config?.metadata?.startTime;
+  const duration = endTime - startTime;
+  
+  console.log('Request to ' + response.config.url + ' took ' + duration + 'ms');
   return response;
 });
 
-const {
-  data: { articles },
-} = await axios.get('/api/data/articles?timeout=3000');
+const response = await axios.get('/api/data/articles?timeout=3000');
+const { data } = response;
 
-document.querySelector('#data').innerHTML = articles[0].content;
+document.querySelector('#data').innerHTML = data[0].content;
